9740a9b47db78f11537bbd6e97888342151c89bb
Luke Wu <lukewu@Lukes-MacBook-Air.local>
2025-10-25 14:36:23 -0500

    idk

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post('/api/upload', async (req, res) => {
 
       // Clean up uploaded files (optional)
       fs.unlinkSync(file1Path);
-      fs.unlinkSync(file2Path);
+      if(file1Path != file2Path) fs.unlinkSync(file2Path);
 
       res.json({
         message: 'Files processed successfully',
